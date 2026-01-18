e1808a6cb5605c9b6b80736b53a7440c205c6cba
nithin <nithinnivi@gmail.com>
2026-01-18 11:28:09 +0530

    update on the sphere

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -1,13 +1,34 @@
 use color::{write_color, Color};
 use ray::Ray;
 use vec3::Point3;
-use vec3::Vec3;
+use vec3::{dot, Vec3};
 
 mod color;
 mod ray;
 mod vec3;
 
-fn ray_color(ray: Ray) -> Color {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = center - r.origin;
+    let a = r.direction.length_squared();
+    let h = dot(r.direction, oc);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = h * h - a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (h - discriminant.sqrt()) / a
+    }
+}
+
+fn ray_color(ray: &Ray) -> Color {
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
+
+    if t > 0.0 {
+        let n = (ray.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
+    }
+
     let unit_direction = ray.direction.unit_vector();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
@@ -24,12 +45,12 @@ fn main() {
 
     // Camera
     let focal_length = 1.0;
-    let viewport_heigth = 2.0;
-    let viewport_width = viewport_heigth * (image_width as f64 / image_height as f64);
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
     let camera_center = Point3::zero();
 
     let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_heigth, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     let pixel_delta_u = viewport_u / image_width as f64;
     let pixel_delta_v = viewport_v / image_height as f64;
@@ -43,23 +64,17 @@ fn main() {
 
     println!("P3\n{image_width} {image_height}\n255");
     for j in 0..image_height {
-        eprint!("\r Scalines remaining: {}", image_height - j);
+        eprint!("\rScanline's remaining: {}", image_height - j);
         for i in 0..image_width {
             let pixel_center =
                 pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
             let ray_direction = pixel_center - camera_center;
             let r = Ray::new(camera_center, ray_direction);
 
-            let pixel_color = ray_color(r);
+            let pixel_color = ray_color(&r);
 
             write_color(&mut out, pixel_color);
         }
     }
     println!("\rDone                        \n")
 }
-
-// Color::new(
-//     i as f64 / (image_width - 1) as f64,
-//     j as f64 / (image_height - 1) as f64,
-//     0.0,
-// );
--- raytrace/src/vec3.rs
+++ raytrace/src/vec3.rs
@@ -31,6 +31,10 @@ impl Vec3 {
     }
 }
 
+pub fn dot(u: Vec3, v: Vec3) -> f64 {
+    u.x * v.x + u.y * v.y + u.z * v.z
+}
+
 impl Add for Vec3 {
     type Output = Vec3;
     fn add(self, other: Vec3) -> Vec3 {
