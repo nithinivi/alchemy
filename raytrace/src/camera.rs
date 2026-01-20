185b2e9c29feb2355f718260c602e2b30b27e69b
nithin <nithinnivi@gmail.com>
2026-01-20 18:59:17 +0530

    add camera class and refactored code

---
+++ raytrace/src/camera.rs
@@ -0,0 +1,90 @@
+//camera.rs
+
+use crate::color::{write_color, Color};
+use crate::hittable::Hittable;
+use crate::interval::Interval;
+use crate::ray::Ray;
+use crate::vec3::{dot, Point3, Vec3};
+
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub image_width: usize,
+
+    //private variables
+    image_height: usize,
+    center: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            aspect_ratio: 1.0,
+            image_width: 100,
+            image_height: 0,
+            center: Point3::new(0.0, 0.0, 0.0),
+            pixel00_loc: Point3::new(0.0, 0.0, 0.0),
+            pixel_delta_u: Vec3::zero(),
+            pixel_delta_v: Vec3::zero(),
+        }
+    }
+
+    pub fn render(&mut self, world: &impl Hittable) {
+        self.initialize();
+        let mut out = std::io::stdout();
+
+        println!("P3\n{} {}\n255", self.image_width, self.image_height);
+        for j in 0..self.image_height {
+            eprint!("\rScanline's remaining: {}", self.image_height - j);
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel00_loc
+                    + (i as f64 * self.pixel_delta_u)
+                    + (j as f64 * self.pixel_delta_v);
+                let ray_direction = pixel_center - self.center;
+                let r = Ray::new(self.center, ray_direction);
+
+                let pixel_color = self.ray_color(&r, world);
+
+                write_color(&mut out, pixel_color);
+            }
+        }
+    }
+
+    pub fn initialize(&mut self) {
+        self.image_height = (self.image_width as f64 / self.aspect_ratio) as usize;
+        self.image_height = if self.image_height < 1 {
+            1
+        } else {
+            self.image_height
+        };
+
+        self.center = Point3::zero();
+
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (self.image_width as f64 / self.image_height as f64);
+
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        self.pixel_delta_u = viewport_u / self.image_width as f64;
+        self.pixel_delta_v = viewport_v / self.image_height as f64;
+
+        let viewport_upper_left =
+            self.center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+
+        self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v);
+    }
+
+    fn ray_color(&self, ray: &Ray, world: &impl Hittable) -> Color {
+        if let Some(rec) = world.hit(ray, Interval::new(0.0, f64::INFINITY)) {
+            return 0.5 * (Color::from(rec.normal) + Color::new(1.0, 1.0, 1.0));
+        }
+
+        let unit_direction = ray.direction.unit_vector();
+        let a = 0.5 * (unit_direction.y + 1.0);
+        (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+    }
+}
